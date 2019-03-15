b21f4e4e2568e0bd8aa37353e62cf3048ccdd9c3
Dekken <philip.deegan@gmail.com>
2019-03-15 14:04:32 +0100

    Update mkn.bat

--- mkn.bat
+++ mkn.bat
@@ -6,5 +6,5 @@ SET VERSION="master"
 SET URL="https://github.com/pybind/pybind11"
 
 IF NOT EXIST %PWD%\p (
-git clone --depth 1 %URL% -b %VERSION% p --recursive || exit 666 /b
+git clone --depth 1 %URL% -b %VERSION% p || exit 666 /b
 )
